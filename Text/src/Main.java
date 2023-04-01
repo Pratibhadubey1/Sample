90dd1298d49ec879da61f508f8b50978586e8838
Pratibhadubey1 <dubeypratibha618@gmail.com>
2023-04-01 13:48:16 +0530

    Changes in main class

--- Text/src/Main.java
+++ Text/src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    void display(){
+        System.out.println("display method");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
